2e0ae2b3a452fe27e6a25a97a85f8cce94314b8d
Soven-ok <47524942+Soven-ok@users.noreply.github.com>
2019-02-20 09:51:48 +0300

    Я не гей
    Это правда

---
+++ GameLabirint/Assets/Scripts/Level4_PickGem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level4_PickGem : MonoBehaviour
+{
+    GameObject gem;
+    public int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
