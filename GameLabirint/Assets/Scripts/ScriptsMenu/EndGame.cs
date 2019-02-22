1890d2e1a0630889d131a3205064fc8d2c5a2499
Soven-ok <47524942+Soven-ok@users.noreply.github.com>
2019-02-22 18:15:01 +0300

    та самая qwe
    и даже rty

--- GameLabirint/Assets/Scripts/ScriptsMenu/EndGame.cs
+++ GameLabirint/Assets/Scripts/ScriptsMenu/EndGame.cs
@@ -5,6 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class EndGame : MonoBehaviour
 {
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
     public void BackToMainMenu()
     {
         SceneManager.LoadScene("MainMenu");
